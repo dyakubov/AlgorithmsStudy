b21c24cebe4f5dbf88dba9c278f86ca1540c1899
dyakubov <tuborg8944@gmail.com>
2019-08-21 17:46:31 +0300

    Savepoint 2

--- src/lesson5/Backpack.java
+++ src/lesson5/Backpack.java
@@ -1,16 +1,76 @@
 package lesson5;
 
+import lesson2.array.Array;
+
 import java.util.*;
 
 public class Backpack {
     private int maxWeight;
     private List<Thing> allThings = new ArrayList<>();
     protected List<Thing> bestSet = new ArrayList<>();
-    private Set<Thing[]> variations = new HashSet<>();
+    private List<List<Thing>> variations = new ArrayList<>();
+
 
     public Backpack(int maxWeight, Thing... things) {
         this.maxWeight = maxWeight;
         this.allThings.addAll(Arrays.asList(things));
+    }
+
+    private void createVariations(){
+        createVariations(allThings.size());
+    }
+
+    private void createVariations(int size){
+        if (size == 0){
+            return;
+        }
+
+        List<Thing> var = new ArrayList<>();
+
+        for (int i = 0; i < size; i++) {
+            var.add(allThings.get(i));
+        }
+
+        createVariations(size - 1);
+        variations.add(var);
+    }
+
+    private void rotate(int length){
+        int pos = allThings.size() - length;
+        Thing temp = allThings.get(pos);
+        for (int i = pos + 1; i < allThings.size(); i++) {
+            allThings.set(pos - 1, allThings.get(pos));
+        }
+
+        allThings.set(allThings.size() - 1, temp);
+    }
+
+    public int getTotalWeight(List<Thing> things){
+        int result = 0;
+        for (Thing thing : things) {
+            result += thing.getWeight();
+        }
+
+        return result;
+    }
+
+    public int getTotalCost(List<Thing> things){
+        int result = 0;
+        for (Thing thing : things) {
+            result += thing.getCost();
+        }
+
+        return result;
+    }
+
+    public void printVariations(){
+        System.out.println(variations.size());
+        for (List<Thing> tSet : variations) {
+            for (Thing t : tSet) {
+                System.out.print(t.getName() + " ");
+            }
+            System.out.println();
+        }
     }
 
     public void getBestSet() {
@@ -20,65 +80,17 @@ public class Backpack {
         } else if (allThings.isEmpty()){
             throw new IllegalArgumentException("Set is empty");
         }
-        createVariations(allThings.size() - 1);
         int maxCost = 0;
-        Thing[] bestVar = new Thing[1];
 
-        for (Thing[] variation : variations) {
+        createVariations();
+        for (List<Thing> variation : variations) {
 
             if (getTotalWeight(variation) <= maxWeight && getTotalCost(variation) >= maxCost) {
                 maxCost = getTotalCost(variation);
-                bestVar = variation;
+                bestSet.clear();
+                bestSet.addAll(variation);
             }
         }
-
-        bestSet.addAll(Arrays.asList(bestVar));
-
-    }
-
-    private void createVariations(int count){
-        if (count == 1){
-            return;
-        }
-
-        for (int i = 0; i < count; i++) {
-            createVariations(count - 1);
-            Thing[] var = new Thing[count];
-            for (int j = 0; j < count; j++) {
-                var[j] = allThings.get(j);
-            }
-            variations.add(var);
-            rotate(count);
-
-        }
-    }
-
-    private void rotate(int count){
-        int pos = allThings.size() - count;
-        Thing temp = allThings.get(pos);
-        for (int i = pos + 1; i < allThings.size(); i++) {
-            allThings.set(pos - 1, allThings.get(pos));
-        }
-
-        allThings.set(allThings.size() - 1, temp);
-    }
-
-    private int getTotalWeight(Thing[] things){
-        int result = 0;
-        for (Thing thing : things) {
-            result += thing.getWeight();
-        }
-
-        return result;
-    }
-
-    private int getTotalCost(Thing[] things){
-        int result = 0;
-        for (Thing thing : things) {
-            result += thing.getCost();
-        }
-
-        return result;
     }
 }
 
--- src/lesson5/BackpackTest.java
+++ src/lesson5/BackpackTest.java
@@ -2,15 +2,16 @@ package lesson5;
 
 public class BackpackTest {
     public static void main(String[] args) {
-        Backpack backpack = new Backpack(8,
-                new Thing("Apple", 2, 1),
+        Backpack backpack = new Backpack(100,
+                new Thing("Apple", 2, 15),
                 new Thing("Player", 3, 50),
-                new Thing("Phone", 4, 100),
-                new Thing("Knife", 5, 15));
+                new Thing("Phone", 4, 60),
+                new Thing("Knife", 5, 15),
+                new Thing("Book", 4, 3)
+                );
 
         backpack.getBestSet();
-        for (Thing thing : backpack.bestSet) {
-            System.out.println(thing.getName());
-        }
+        backpack.printVariations();
+
     }
 }
